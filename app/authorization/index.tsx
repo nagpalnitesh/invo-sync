82db9f238ab8fc5810be1ba7c47f547de02e7c89
Nitesh Nagpal <niteshnagpal@outlook.com>
2025-01-12 01:25:04 +0530

    login screen

--- app/authorization/index.tsx
+++ app/authorization/index.tsx
@@ -1,6 +1,6 @@
 import { router, useNavigation } from 'expo-router';
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import '../../global.css';
@@ -12,14 +12,25 @@ const Login = () => {
     }
     return (
         <Container>
-            <View className='p-2 gap-5'>
+            <View className='p-2 gap-5 flex-1 justify-center'>
+                <View className='gap-2'>
+                    <View className='items-center'>
+                        <Image source={require('../../assets/logo1.png')} className="w-32 h-32"
+                            resizeMode="contain" />
+                    </View>
+                    <View className='items-center'>
+                        <Text className="text-3xl font-bold text-gray-800">Welcome to InvoSync</Text>
+                    </View>
+                </View>
                 <View className='flex-row justify-center items-center gap-1'>
                     <Text className='text-xl text-dark font-normal'>Don't have an account?</Text>
                     <TouchableOpacity onPress={() => onSignup()} className=''>
                         <Text className='text-dark text-xl font-bold text-center uppercase'>Signup</Text>
                     </TouchableOpacity>
                 </View>
-                <Button title='Login' variant='primary' className='mt-auto w-5/6 mx-auto' onPress={() => navigation.navigate('dashboard' as never)} />
+                <View className='w-full'>
+                    <Button title='Login' variant='primary' className='mt-auto w-5/6 mx-auto' onPress={() => navigation.navigate('dashboard' as never)} />
+                </View>
             </View>
         </Container>
     )
